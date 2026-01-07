bcb509e84579a9f4af43b0006526091b37c164bd
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-01-07 18:44:14 +0100

    initial commit - Next.js practice 01

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Header from "@/components/header.js";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
+    </main>
+  );
+}
